aeb58824eaaeed9e45ff9e8e0bd5248f46ddb5da
Erasmo-Veloso <creatorerasmo@gmail.com>
2026-01-03 15:50:32 +0100

    fix: deploy error 01

--- db/index.ts
+++ db/index.ts
@@ -1,13 +1,27 @@
 import { drizzle as drizzleNeon } from 'drizzle-orm/neon-http'
 import { drizzle as drizzlePostgres } from 'drizzle-orm/node-postgres'
 import { neon } from '@neondatabase/serverless'
+import { Pool } from 'pg'
 
 import * as schema from './schema'
 
-export const db = process.env.VERCEL
+const isVercel = !!process.env.VERCEL
+
+export const db = isVercel
   ? drizzleNeon({
       client: neon(process.env.DATABASE_URL!),
       schema,
       casing: 'snake_case',
     })
-  : drizzlePostgres(process.env.DATABASE_URL!, { schema, casing: 'snake_case' })
+  : drizzlePostgres(
+      new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+      {
+        schema,
+        casing: 'snake_case',
+      }
+    )
