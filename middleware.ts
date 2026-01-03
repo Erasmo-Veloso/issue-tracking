b8c07724560005a21abeb1151ad1946dd4571348
Erasmo-Veloso <creatorerasmo@gmail.com>
2026-01-03 15:13:23 +0100

    feat: Config && Middleware

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export const middleware = (request: NextRequest) => {
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ["/api/issue/:path*"],
+};
